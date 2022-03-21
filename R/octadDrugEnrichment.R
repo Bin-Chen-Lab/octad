ff0c954c5fadec2bbdccef010cb154426e911756
Lionir <eygen.chekalin@gmail.com>
2022-03-21 11:56:19 -0500

    0.46 octadDrugEnrichment bux fix

--- R/octadDrugEnrichment.R
+++ R/octadDrugEnrichment.R
@@ -40,21 +40,32 @@ for(target_type_selected in target_type){
     drug_pred = sRGES
 
     rgess = matrix(-1*drug_pred$sRGES, ncol = 1)
-    rownames(rgess) = drug_pred$pert_iname
-    gsea_results = GSVA::gsva(rgess, cmpdSets, method = "ssgsea",    parallel.sz=8,ssgsea.norm = TRUE,verbose=FALSE)
+	
+	if(is.null(dim(rgess))){
+		warning('rgess have zero rows, recompute it and try again')
+		next
+	} else{
+	
+		rownames(rgess) = drug_pred$pert_iname
+		gsea_results = GSVA::gsva(rgess, cmpdSets, method = "ssgsea",    parallel.sz=8,ssgsea.norm = TRUE,verbose=FALSE)
 
-    gsea_results = merge(random_gsea_score[[target_type_selected]], gsea_results,by='row.names')
-    row.names(gsea_results)=gsea_results$Row.names
-    gsea_results$Row.names=NULL
-    gsea_summary = data.frame(score = gsea_results[,ncol(random_gsea_score[[target_type_selected]])+1])
+		gsea_results = merge(random_gsea_score[[target_type_selected]], gsea_results,by='row.names')
+		
+			if(is.null(dim(gsea_results))){
+		warning('gsea_results have zero rows, recompute it and try again')
+		next
+		} else{
+		rownames(gsea_results)=gsea_results$Row.names
+		gsea_results$Row.names=NULL
+		gsea_summary = data.frame(score = gsea_results[,ncol(random_gsea_score[[target_type_selected]])+1])
 #calculating p.value
-    gsea_p = apply(gsea_results, 1, function(x){
-        sum(x[seq_len(ncol(random_gsea_score[[target_type_selected]]))] > x[ncol(random_gsea_score[[target_type_selected]])+1])/ncol(random_gsea_score[[target_type_selected]])
+		gsea_p = apply(gsea_results, 1, function(x){
+			sum(x[seq_len(ncol(random_gsea_score[[target_type_selected]]))] > x[ncol(random_gsea_score[[target_type_selected]])+1])/ncol(random_gsea_score[[target_type_selected]])
     })
-    gsea_p = data.frame(target = names(gsea_p),score = gsea_summary, p = gsea_p, padj = p.adjust(gsea_p, method = 'fdr'))
-    gsea_p = gsea_p[order(gsea_p$padj), ]
-    write.csv(gsea_p, paste0(enrichFolder.n, "/enriched_", target_type_selected, ".csv"),row.names = FALSE)
-    top.out.num = nrow(gsea_p[which(gsea_p$padj<=0.05),])
+		gsea_p = data.frame(target = names(gsea_p),score = gsea_summary, p = gsea_p, padj = p.adjust(gsea_p, method = 'fdr'))
+		gsea_p = gsea_p[order(gsea_p$padj), ]
+		write.csv(gsea_p, paste0(enrichFolder.n, "/enriched_", target_type_selected, ".csv"),row.names = FALSE)
+		top.out.num = nrow(gsea_p[which(gsea_p$padj<=0.05),])
     if (top.out.num == 0) {
         top.out.num = 1
     }
@@ -87,6 +98,6 @@ if(nrow(gsea_p)>0){
     }
     cat(paste('Done for',target_type_selected,'for',nrow(gsea_p[which(gsea_p$padj<=0.05),]),'genes'),'\n')
     }else cat(paste('No signigicant enrichment found for',target_type_selected),'\n')
-}
+}}}
 options(warn=0)
     }
